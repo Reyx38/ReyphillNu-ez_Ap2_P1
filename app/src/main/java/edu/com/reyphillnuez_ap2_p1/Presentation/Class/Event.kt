88d4760df995c47971a8f9699a633cf3015e4edc
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-05-28 20:45:48 -0400

    Agregando Funcionalidad inicial

---
+++ app/src/main/java/edu/com/reyphillnuez_ap2_p1/Presentation/Class/Event.kt
@@ -0,0 +1,5 @@
+package edu.com.reyphillnuez_ap2_p1.Presentation.Class
+
+sealed interface Event {
+
+}
---
+++ app/src/main/java/edu/com/reyphillnuez_ap2_p1/Presentation/Class/UiState.kt
@@ -0,0 +1,5 @@
+package edu.com.reyphillnuez_ap2_p1.Presentation.Class
+
+data class UiState (
+    val ClaseId: Int? = null
+)
