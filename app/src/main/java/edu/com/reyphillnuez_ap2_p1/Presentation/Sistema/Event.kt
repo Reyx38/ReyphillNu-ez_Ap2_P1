42c6e768e37fd64b8e4d1c22ad643504a7a4c1de
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-05-28 20:50:11 -0400

    Modificacion funcionalidad principal

---
+++ app/src/main/java/edu/com/reyphillnuez_ap2_p1/Presentation/Sistema/Event.kt
@@ -0,0 +1,5 @@
+package edu.com.reyphillnuez_ap2_p1.Presentation.Sistema
+
+sealed interface Event {
+
+}
---
+++ app/src/main/java/edu/com/reyphillnuez_ap2_p1/Presentation/Sistema/Sistema.java
@@ -0,0 +1,4 @@
+package edu.com.reyphillnuez_ap2_p1.Presentation.Sistema;
+
+public class Sistema {
+}
