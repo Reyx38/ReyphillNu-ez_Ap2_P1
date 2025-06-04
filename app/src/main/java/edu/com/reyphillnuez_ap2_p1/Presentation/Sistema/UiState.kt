391d91b593a16b7eced71b9a42a247e48d5a6e62
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-06-04 18:59:06 -0400

    UiState

--- app/src/main/java/edu/com/reyphillnuez_ap2_p1/Presentation/Sistema/UiState.kt
+++ app/src/main/java/edu/com/reyphillnuez_ap2_p1/Presentation/Sistema/UiState.kt
@@ -1,5 +1,12 @@
 package edu.com.reyphillnuez_ap2_p1.Presentation.Sistema
 
+import edu.com.reyphillnuez_ap2_p1.Data.Local.entities.TareasEntity
+
 data class UiState (
-    val ClaseId: Int? = null
+    val tareasId: Int? = null,
+    val descripcion: String? = null,
+    val errorDescripcion: String? = null,
+    val tiempo: Int? = null,
+    val errorTiempo: String? = null,
+    val tareas: List<TareasEntity> = emptyList()
 )
