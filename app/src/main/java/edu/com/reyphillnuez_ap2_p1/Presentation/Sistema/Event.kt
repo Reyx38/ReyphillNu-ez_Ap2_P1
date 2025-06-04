836ac7b8dc0cb8120eaa955895d13867e79541bc
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-06-04 18:58:56 -0400

    Evento

--- app/src/main/java/edu/com/reyphillnuez_ap2_p1/Presentation/Sistema/Event.kt
+++ app/src/main/java/edu/com/reyphillnuez_ap2_p1/Presentation/Sistema/Event.kt
@@ -1,5 +1,10 @@
 package edu.com.reyphillnuez_ap2_p1.Presentation.Sistema
 
 sealed interface Event {
-
+    data class onChangeTareasId(val id: Int?): Event
+    data class onChangeDescripcion(val descripcion: String): Event
+    data class onChangeTiempo(val tiempo: Int?): Event
+    data object Save: Event
+    data object Delete: Event
+    data object New: Event
 }
