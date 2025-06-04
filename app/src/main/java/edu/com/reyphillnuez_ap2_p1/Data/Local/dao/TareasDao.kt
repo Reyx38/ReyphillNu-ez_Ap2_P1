00e2a7f003c549aa512e34fa3957961a71593861
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-06-04 18:58:01 -0400

    Dao

---
+++ app/src/main/java/edu/com/reyphillnuez_ap2_p1/Data/Local/dao/TareasDao.kt
@@ -0,0 +1,25 @@
+package edu.com.reyphillnuez_ap2_p1.Data.Local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.com.reyphillnuez_ap2_p1.Data.Local.entities.TareasEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TareasDao {
+    @Upsert
+    suspend fun save(taskEntity: TareasEntity)
+    @Query(
+        """
+            Select * from
+            Tareas where tareaId=:id
+        """
+    )
+    suspend fun find(id: Int?)
+    @Delete
+    suspend fun delete(taskEntity: TareasEntity)
+    @Query("""select * from tareas""")
+    fun getAll(): Flow<List<TareasEntity>>
+}
