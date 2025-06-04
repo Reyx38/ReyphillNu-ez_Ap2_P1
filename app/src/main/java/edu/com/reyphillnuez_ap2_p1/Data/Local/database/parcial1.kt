785c9076a732ada4511d12b698173d4ec3b3ae07
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-06-04 18:58:47 -0400

    base de datos

---
+++ app/src/main/java/edu/com/reyphillnuez_ap2_p1/Data/Local/database/parcial1.kt
@@ -0,0 +1,18 @@
+package edu.com.reyphillnuez_ap2_p1.Data.Local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.com.reyphillnuez_ap2_p1.Data.Local.dao.TareasDao
+import edu.com.reyphillnuez_ap2_p1.Data.Local.entities.TareasEntity
+
+@Database(
+    entities =[
+        TareasEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class parcial1: RoomDatabase()
+{
+    abstract fun tareasDao(): TareasDao
+}
