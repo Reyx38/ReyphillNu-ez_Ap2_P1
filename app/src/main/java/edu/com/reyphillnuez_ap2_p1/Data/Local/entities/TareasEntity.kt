e8b2fb86a8fcfcceed9b4e99e9f4536da1fbff35
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-06-04 20:29:39 -0400

    tareas entity

--- app/src/main/java/edu/com/reyphillnuez_ap2_p1/Data/Local/entities/TareasEntity.kt
+++ app/src/main/java/edu/com/reyphillnuez_ap2_p1/Data/Local/entities/TareasEntity.kt
@@ -1,9 +1,11 @@
 package edu.com.reyphillnuez_ap2_p1.Data.Local.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "Tareas")
 data class TareasEntity (
+    @PrimaryKey
     val tareaId: Int? = null,
     val descripcion: String? = null,
     val tiempo: Int? = null
