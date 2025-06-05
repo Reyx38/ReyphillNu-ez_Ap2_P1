c0c6de51602b7c710a3813ec133c97cd22933b92
Reyphill Nuñez <reyphillduarte06@gmail.com>
2025-06-04 20:28:36 -0400

    NAVIGATION

--- app/src/main/java/edu/com/reyphillnuez_ap2_p1/Presentation/Navigation/HostNavigation.kt
+++ app/src/main/java/edu/com/reyphillnuez_ap2_p1/Presentation/Navigation/HostNavigation.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import edu.com.reyphillnuez_ap2_p1.Presentation.Sistema.List
+import androidx.navigation.toRoute
+import edu.com.reyphillnuez_ap2_p1.Presentation.Sistema.TareasList
+import edu.com.reyphillnuez_ap2_p1.Presentation.Sistema.TareasScreen
 
 @Composable
 fun HostNavigation(
@@ -16,7 +18,25 @@ fun HostNavigation(
         startDestination = Screen.List
     ) {
         composable<Screen.List> {
-            List()
+            TareasList(
+                createTarea = {
+                    navHostController.navigate(Screen.Tarea(0))
+                },
+                onEdit = { tareaId ->
+                    navHostController.navigate(Screen.Tarea(tareaId))
+                }
+            )
+        }
+
+        composable<Screen.Tarea> { backStack ->
+            val tareaId = backStack.toRoute<Screen.Tarea>().id
+            TareasScreen(
+                tareaId = tareaId,
+                goBack = {
+                    navHostController.navigateUp()
+                }
+            )
+
         }
     }
 }
